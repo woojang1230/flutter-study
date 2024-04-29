10ed2be111d2c90b1e52556d8b6cbc8b22232fc4
woojang1230 <woojang1230@gmail.com>
2024-04-30 00:47:23 +0900

    feat: 클래스 분리 및 Padding 적용

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,33 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Color(0xFF335CB0),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset(
-            'asset/img/logo.png',
-          ),
-          CircularProgressIndicator(
-            color: Colors.white,
-          )
-        ],
-      ),
-    ),
+    home: HomeScreen()
   ));
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF335CB0),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 32.0
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'asset/img/logo.png',
+            ),
+            CircularProgressIndicator(
+              color: Colors.white,
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
